a0dff5032aac779c69396fb819f5f13b139bcc46
Jeandre Visser <jeandre@nutrimeals.ca>
2023-04-06 23:04:05 -0600

    Add button

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import classes from './event-item.module.css';
+import Button from '../ui/button';
 
 const EventItem = (props) => {
   const humanReadableDate = new Date(props.date).toLocaleDateString('en-US', {
@@ -26,7 +27,7 @@ const EventItem = (props) => {
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>Explore Event</Button>
         </div>
       </div>
     </li>
