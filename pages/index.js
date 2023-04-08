c77d1ead9e5e1a9c3832e73b13479f048f22901e
Jeandre Visser <jeandre@nutrimeals.ca>
2023-04-08 13:57:42 -0600

    Use getStaticProps to render data from firebase

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-utils';
 import EventList from '@/components/events/event-list';
 
-export default function HomePage() {
-  const featuredEvents = getFeaturedEvents();
-
+export default function HomePage(props) {
   return (
     <div>
       <ul>
-        <EventList items={featuredEvents} />
+        <EventList items={props.events} />
       </ul>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
