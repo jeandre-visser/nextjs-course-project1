e4f74366941c6fa8f7045328e571797d144d9015
Jeandre Visser <jeandre@nutrimeals.ca>
2023-04-12 21:06:27 -0600

    Connect to mongodb

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -7,6 +9,15 @@ function handler(req, res) {
       return;
     }
 
+    // Store it in a database
+    const client = await MongoClient.connect(
+      'mongodb+srv://jeandrev14:Password21@cluster-nextjs.0z2aj6o.mongodb.net/newsletter?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+    const newsletterCollection = db.collection('emails');
+    await newsletterCollection.insertOne({ email: userEmail });
+
+    client.close();
     console.log(userEmail);
 
     res.status(201).json({ message: 'Signed up!' });
